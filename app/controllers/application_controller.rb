f17f9e5c3a5bbfda0df6476a61d1f359cde4b675
cory321 <cory321@gmail.com>
2015-11-02 22:55:55 -0800

    added navbar login/logout logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,15 @@ class ApplicationController < ActionController::Base
   	end
   end
 
-def confirm_logged_in
-   	 redirect_to login_path, alert: "Please Log In" unless session[:user_id]
-end
+	def confirm_logged_in
+	   	 redirect_to login_path, alert: "Please Log In" unless session[:user_id]
+	end
+
+	def current_user
+		return unless session[:user_id]
+		@current_user ||= User.find_by_id(session[:user_id])
+	end
+
+  helper_method :current_user #this makes it available for the view
 
 end
