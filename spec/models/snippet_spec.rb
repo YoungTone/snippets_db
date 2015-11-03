a8745fe67d36e43962217b7d1fb92c9b38dec0eb
Jorie Clark <jorie.clark@gmail.com>
2015-11-02 19:02:52 -0800

    Add snippet tests

---
+++ spec/models/snippet_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Snippet, type: :model do
+  it { is_expected.to respond_to :name }
+  it { is_expected.to respond_to :editor }
+  it { is_expected.to respond_to :description }
+  it { is_expected.to respond_to :code }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :editor }
+  it { is_expected.to validate_presence_of :description }
+  it { is_expected.to validate_presence_of :code }
+  it { is_expected.to validate_uniqueness_of :code }
+  it { is_expected.to validate_length_of :code }
+  it { is_expected.to belong_to :user }
+  xit { is_expected.to have_many :tags }
+end
