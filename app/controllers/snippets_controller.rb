2bce4e1d399dce263b4780f59b91e28f8eb21db5
Jorie Clark <jorie.clark@gmail.com>
2015-11-03 15:30:08 -0800

    Create snippet controller

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,13 +1,50 @@
 class SnippetsController < ApplicationController
-  def index
-  end
+	def index
+		@snippets = Snippet.all
+		render :index
+	end
 
-  def show
-  end
+	def new
+		@snippet = Snippet.new
+	end
 
-  def new
-  end
+	def create
+		@snippet = Snippet.new(snippet_params)
+		if @snippet.save
+			redirect_to user_snippet_path[:user_id], flash: {success: "Thanks for Contributing"}
+		else
+			render :new
+		end
+	end
 
-  def edit
-  end
+	def show
+		@snippet = Snippet.find params[:id]
+	end
+
+	def edit
+		@snippet = Snippet.find params[:id]
+	end
+
+	def update
+		@snippet = Snippet.find params[:id]
+		@snippet.update snippet_params
+		redirect_to snippets_path
+		if @snippet.save
+			redirect_to snippet_path(@snippet), notice: "Updated"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@snippet = Snippet.find_by_id params[:id]
+		@snippet.destroy
+		redirect_to snippets_path
+	end
+
+	private
+
+	def snippet_params
+		params.require(:snippet).permit(:name, :code, :description, :user_id, :language_id, :editor_id)
+	end
 end
