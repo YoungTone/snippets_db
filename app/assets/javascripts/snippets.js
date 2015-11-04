1da16c95a0e34da2aac086b754035cf820c31549
cory321 <cory321@gmail.com>
2015-11-03 22:49:59 -0800

    created mime-types array, solved js dom-ready load conflict with turbolinks

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -1,21 +1,49 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-
-function render_code_mirror(mode) {
+$(function(){
+	
 	var cm;
+
+	var special_mimetypes = {
+		"c" : "text/x-csrc",
+		"c++" : "text/x-c++src",
+		"c#" : "text/x-csharp",
+		"objective-c" : "text/x-objectivec",
+		"scala" : "text/x-scala",
+		"java" : "text/x-java",
+		"scss" : "css",
+		"less" : "css",
+		"sass" : "css",
+		"angular.js" : "javascript",
+		"backbone.js" : "javascript",
+		"ember.js" : "javascript",
+		"knockout.js" : "javascript",
+		"react.js" : "javascript",
+		"jquery" : "javascript",
+		"wrapped php" : "php"
+	};
 
 	$("#codemirror").append(function() {
 			cm = CodeMirror.fromTextArea($(this).get(0), {
-  			lineNumbers: true,
-  			mode: 'javascript'
-		});
+				theme: "monokai",
+  				lineNumbers: true
+			});
+	});
+		
+	$("select").on('change', function(){
+		var selection = $(this).find('option:selected').val().toLowerCase();
+
+		if(special_mimetypes[selection]) {
+			selection = special_mimetypes[selection];
+		}
+		
+		cm.setOption("mode", selection);
 	});
 
-	$('.btn-default').on('click', function(e){
-		e.preventDefault();
-		cm.save();
-		console.log($("#codemirror").val());
-	});
-}
+	$('.snippet-form').submit(function() {
+        cm.save();
+        return true; // return false to cancel form action
+    });
 
+});
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,6 @@
 class Language < ActiveRecord::Base
   has_many :snippets
-  validates :highlight_class, :name, presence: true
+  validates :mode, :name, presence: true
   validates :name, uniqueness: true
 
 end
